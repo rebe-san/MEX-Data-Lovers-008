07dea7bb233aba3ca110ade8b2e2b8e0bb4dea45
Paola Torres <paola.t.betancourt@gmail.com>
2019-07-05 11:42:19 -0500

    Se agrego select a la welcome.html

--- src/welcome.js
+++ src/welcome.js
@@ -1,3 +1,22 @@
-document.getElementById("buttonAppi").addEventListener("click", ()=>{
+document.getElementById("buttonSearch").addEventListener("click", ()=>{
+  location.href = "appi.html"
+})
+  document.getElementById("name").addEventListener("click", ()=>{
+    location.href = "appi.html"
+  })
+
+  document.getElementById("upward").addEventListener("click", ()=>{
+    location.href = "appi.html"
+  })
+
+  document.getElementById("falling").addEventListener("click", ()=>{
+    location.href = "appi.html"
+  })
+
+  document.getElementById("optionA").addEventListener("click", ()=>{
+    location.href = "appi.html"
+  })
+
+  document.getElementById("optionB").addEventListener("click", ()=>{
     location.href = "appi.html"
   })
